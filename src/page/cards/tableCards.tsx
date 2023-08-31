4d533220193f7bb4b58c343b5eec907332e46146
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-09-01 01:42:55 +0300

    refactor

--- src/page/cards/tableCards.tsx
+++ src/page/cards/tableCards.tsx
@@ -19,12 +19,12 @@ export const TableCards = () => {
   const currentUserId = useSelector((state: RootState) => state.auth.currentUserId)
   const { data } = useGetCardsQuery(id ?? '')
   let cardsItems: CardsItemsType[] = []
-  let emptyDeck = false
+  let deckWithCard = true
 
   if (data) {
     cardsItems = data.items
     if (data.items.length === 0) {
-      emptyDeck = true
+      deckWithCard = false
     }
   }
   const handlerOnClick = () => {
@@ -36,69 +36,73 @@ export const TableCards = () => {
     setValueInput('')
   }
 
-  return emptyDeck ? (
-    <div className={st.common}>
-      <div className={st.blockArrowBack} onClick={handlerOnClick}>
-        <IconArrowBack />
-        <Typography variant={'body2'}>Back to Packs List</Typography>
-      </div>
-      <div className={st.blokButtonAddNewCard}>
-        <Typography variant={'large'}>Empty Pack</Typography>
-      </div>
-      {currentUserId === myUserId && <Button variant={'primary'}>Add New Card</Button>}
-    </div>
-  ) : (
-    <div className={st.common}>
-      <div className={st.blockArrowBack} onClick={handlerOnClick}>
-        <IconArrowBack />
-        <Typography variant={'body2'}>Back to Packs List</Typography>
-      </div>
-      {myUserId === data?.items[0].userId ? (
-        <div className={st.blokButtonAddNewCard}>
-          <Typography variant={'large'}>My Pack</Typography>
-          <Button variant={'primary'}>Add New Card</Button>
+  if (data) {
+    return deckWithCard ? (
+      <div className={st.common}>
+        <div className={st.blockArrowBack} onClick={handlerOnClick}>
+          <IconArrowBack />
+          <Typography variant={'body2'}>Back to Packs List</Typography>
         </div>
-      ) : (
-        <div className={st.blokButtonAddNewCard}>
-          <Typography variant={'large'}>Friend’s Pack</Typography>
-          <Button variant={'primary'}>Learn to Pack</Button>
-        </div>
-      )}
+        {myUserId === data?.items[0].userId ? (
+          <div className={st.blokButtonAddNewCard}>
+            <Typography variant={'large'}>My Pack</Typography>
+            <Button variant={'primary'}>Add New Card</Button>
+          </div>
+        ) : (
+          <div className={st.blokButtonAddNewCard}>
+            <Typography variant={'large'}>Friend’s Pack</Typography>
+            <Button variant={'primary'}>Learn to Pack</Button>
+          </div>
+        )}
 
-      <div className={st.textField}>
-        <TextField
-          sizeWidthTextField={'4000px'}
-          handlerOnChange={handlerSendInputValue}
-          valueInput={valueInput}
-          setValueInput={setValueInput}
-          placeholder={'Input search'}
-          type="text"
-          showIconClose={false}
-          label={'Write a symbol and  press еnter'}
-        />
-      </div>
-      <table className={st.table}>
-        <thead>
-          <tr className={st.header}>
-            {DATA_HEADERS_TABLE_CARDS.map(el => (
-              <th key={el.key} className={st.thHeader}>
-                {el.title}
-              </th>
-            ))}
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cardsItems?.map((deck: CardsItemsType) => (
-            <tr key={deck.id} className={st.tr}>
-              <td className={st.tdCommonStyle}>{deck.question}</td>
-              <td className={st.tdCommonStyle}>{deck.answer}</td>
-              <td className={st.tdCommonStyle}>{new Date(deck.updated).toLocaleString('ru-Ru')}</td>
-              <td className={st.tdCreatedBy}>{deck.grade}</td>
+        <div className={st.textField}>
+          <TextField
+            sizeWidthTextField={'4000px'}
+            handlerOnChange={handlerSendInputValue}
+            valueInput={valueInput}
+            setValueInput={setValueInput}
+            placeholder={'Input search'}
+            type="text"
+            showIconClose={false}
+            label={'Write a symbol and  press еnter'}
+          />
+        </div>
+        <table className={st.table}>
+          <thead>
+            <tr className={st.header}>
+              {DATA_HEADERS_TABLE_CARDS.map(el => (
+                <th key={el.key} className={st.thHeader}>
+                  {el.title}
+                </th>
+              ))}
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+          </thead>
+          <tbody>
+            {cardsItems?.map((deck: CardsItemsType) => (
+              <tr key={deck.id} className={st.tr}>
+                <td className={st.tdCommonStyle}>{deck.question}</td>
+                <td className={st.tdCommonStyle}>{deck.answer}</td>
+                <td className={st.tdCommonStyle}>
+                  {new Date(deck.updated).toLocaleString('ru-Ru')}
+                </td>
+                <td className={st.tdCreatedBy}>{deck.grade}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    ) : (
+      <div className={st.common}>
+        <div className={st.blockArrowBack} onClick={handlerOnClick}>
+          <IconArrowBack />
+          <Typography variant={'body2'}>Back to Packs List</Typography>
+        </div>
+        <div className={st.blokButtonAddNewCard}>
+          <Typography variant={'large'}>Empty Pack</Typography>
+        </div>
+        {currentUserId === myUserId && <Button variant={'primary'}>Add New Card</Button>}
+      </div>
+    )
+  }
 }
