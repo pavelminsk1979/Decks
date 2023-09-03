d8df9e239f6ef36bdd3dbd99e56a6b2f52f22386
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-09-03 17:25:34 +0300

    refactor Button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType, forwardRef, ReactNode } from 'react'
 
 import s from './button.module.scss'
 
@@ -10,37 +10,18 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   className?: string
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+export const Button = forwardRef(
+  <T extends ElementType = 'button'>(props: ButtonProps<T>, ref: any) => {
+    const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
 
-  return (
-    <Component
-      className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className} ${s.button} ${
-        s.primary
-      }`}
-      {...rest}
-    />
-  )
-}
-
-/*import { ComponentPropsWithoutRef, ElementType } from 'react'
-
-import s from './button.module.scss'
-
-export type ButtonProps<T extends ElementType = 'button'> = {
-  as?: T
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
-  fullWidth?: boolean
-  className?: string
-  test: boolean
-} & ComponentPropsWithoutRef<T>
-
-export const Button = <T extends ElementType = 'button'>(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
-) => {
-  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
-
-  return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
-  )
-}*/
+    return (
+      <Component
+        ref={ref}
+        className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className} ${s.button} ${
+          s.primary
+        }`}
+        {...rest}
+      />
+    )
+  }
+)
