063b13605af49c4a8398bbd040fcb03e58b89a9d
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-09-07 04:18:11 +0300

    Learn Work!

--- src/page/learn/learn.tsx
+++ src/page/learn/learn.tsx
@@ -27,7 +27,10 @@ export const Learn = () => {
   const { data } = useGetRandomCardQuery(id ?? '')
   const [updateGradeCard, { data: dataUpdateGrade }] = useUpdateGradeCardsMutation()
   const [mode, setMode] = useState(false)
-  let [currentQuestion, setCurrentQuestion] = useState(data?.question)
+  let [currentQuestion, setCurrentQuestion] = useState('')
+  let [currentShots, setCurrentShots] = useState(0)
+  let [currentAnswer, setCurrentAnswer] = useState('')
+  let [currentIdCard, setCurrentIdCard] = useState('')
 
   const handlerOnClickBackPage = () => {
     navigate('/decks')
@@ -36,6 +39,9 @@ export const Learn = () => {
     setMode(!mode)
     if (dataUpdateGrade) {
       setCurrentQuestion(dataUpdateGrade.question)
+      setCurrentShots(dataUpdateGrade.shots)
+      setCurrentAnswer(dataUpdateGrade.answer)
+      setCurrentIdCard(dataUpdateGrade.id)
     }
   }
   const stateRadioGroup: ElementsRadioType[] = [
@@ -46,16 +52,12 @@ export const Learn = () => {
     { id: '5', text: 'Knew the answer', name: 'trainGroup', disabled: false },
   ]
   const handlerCallbackRadioGroup = (value: string) => {
-    const body = { cardId: data ? data.id : '', grade: Number(value) }
+    const body = { cardId: currentIdCard ? currentIdCard : data?.id!, grade: Number(value) }
 
-    updateGradeCard({ id: id ? id : '', body })
+    if (id !== undefined && body.cardId !== undefined) {
+      updateGradeCard({ id, body })
+    }
   }
-
-  console.log(data)
-  console.log(dataUpdateGrade, '66666')
-
-  let currentGrade = data?.grade
-  let currentAnswer = 'JJJJJJJJJJJ'
 
   return (
     <div className={st.common}>
@@ -67,16 +69,20 @@ export const Learn = () => {
         <Typography variant={'large'}>{`Learn : "${currentNameDack}"`}</Typography>
         <div className={st.question}>
           <Typography variant={'subtitle1'}>Qusetion: </Typography>
-          <Typography variant={'body1'}>{currentQuestion}</Typography>
+          <Typography variant={'body1'}>
+            {currentQuestion ? currentQuestion : data?.question}
+          </Typography>
         </div>
         <div className={mode ? st.amount : st.amountMode}>
           <Typography variant={'body2'}>Количество попыток ответов на вопрос:</Typography>
-          <Typography variant={'subtitle2'}>{currentGrade}</Typography>
+          <Typography variant={'subtitle2'}>{currentShots ? currentShots : data?.grade}</Typography>
         </div>
         {mode && (
           <div className={st.answer}>
             <Typography variant={'subtitle1'}>Answer: </Typography>
-            <Typography variant={'body1'}>{currentAnswer}</Typography>
+            <Typography variant={'body1'}>
+              {currentAnswer ? currentAnswer : data?.answer}
+            </Typography>
           </div>
         )}
 
